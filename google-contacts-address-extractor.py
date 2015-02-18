492e2f068455ef10fcf0a9401c30e5c99b91b6ad
desko27 <desko27@gmail.com>
2015-02-18 19:44:33 +0100

    command line interface, 'per account' output files option

--- google-contacts-address-extractor.py
+++ google-contacts-address-extractor.py
@@ -3,14 +3,29 @@
 # ---------------------------------------------------------------------------
 #  - Author:    desko27
 #  - Email:     desko27@gmail.com
-#  - Version:   1.0.2
+#  - Version:   1.1.0
 #  - Created:   2015/01/28
-#  - Updated:   2015/02/08
+#  - Updated:   2015/02/18
 # ----------------------------------------------------------------------------
-# This script is intended for extracting contact's addresses from a specified
-# set of Google Apps users. You must have Google Apps with API enabled, and
-# set up the API Auth data on the corresponding file.
+"""This script is intended for extracting contact's addresses from a specified
+set of Google Apps users. You must have Google Apps with API enabled, and
+set up the API Auth data on the corresponding file.
 
+Output goes to the relative folder defined in results_folder value of conf.ini
+
+Usage: ~extractor.py [-s] [-k] (-f | <source-accounts> ...)
+  
+Options:
+  -h --help
+  -s --separated  send output to separated files (per account)
+  -k --keep       keep previous results on the output folder
+  -f --from-file  get source accounts from file instead of from arguments
+  
+"""
+
+from docopt import docopt
+from os import listdir, remove
+from os.path import join, isfile
 from sys import maxint
 
 # google api
@@ -26,15 +41,24 @@ from class_Config import Config, conf_exists
 # program
 # ---------------------------------------------------------------------------
 if __name__ == '__main__':
+
+	# retrieve arguments
+	args = docopt(__doc__)
 	
 	# retrieve config values
 	files = Config('conf.ini').files
 	auth = Config(files.google_apps_api_auth).auth
 	
+	# remove previous results on results folder
+	if not args['--keep']:
+		for f in listdir(files.results_folder):
+			element = join(files.results_folder, f)
+			if isfile(element): remove(element)
+	
 	# list managers
 	lm_exclusions = ListManager(file = files.exclusions)
 	lm_source = ListManager(file = files.source_accounts)
-	lm_export = ListManager(file = files.extracted_addresses, load = False)
+	lm_export = ListManager(file = join(files.results_folder, files.results_file), load = False)
 	
 	# google auth
 	google = GoogleAuth(auth.consumer_key, auth.consumer_secret)
@@ -44,7 +68,7 @@ if __name__ == '__main__':
 	query.max_results = maxint
 	
 	# load & iterate over accounts
-	accounts = lm_source.load()
+	accounts = lm_source.load() if args['--from-file'] else args['<source-accounts>']
 	exclusions = lm_exclusions.load()
 	
 	print 'Processing accounts...\n'
@@ -68,14 +92,17 @@ if __name__ == '__main__':
 				else:
 					addresses.append(email.address)
 					
-		addresses = list(set(addresses)) # unique elements
+		lm_acc_export = ListManager(file = join(files.results_folder, '%s.txt' % account), load = False)
+		lm_acc_export.list = addresses
+		lm_acc_export.unique_elements()
+		if args['--separated']: lm_acc_export.save()
 		
 		print '(%i)' % len(addresses)
 		lm_export.list += addresses
 	
-	# save the extracted addresses
+	# save the total extracted addresses
 	lm_export.unique_elements()
-	lm_export.save()
+	if not args['--separated']: lm_export.save()
 	
 	# finished
 	print '\nFinished! - (%s) TOTAL unique addresses.' % len(lm_export.list)
